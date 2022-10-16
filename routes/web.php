ec92d5cdf5f85a727fcbd5bd686121ad0a358c82
huizai1 <237002893@qq.com>
2022-10-16 14:52:15 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 // 用户资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
