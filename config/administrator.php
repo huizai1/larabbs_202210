ed53c57c42f449419fea06394c3d5ed9f3c68d1c
huizai1 <237002893@qq.com>
2022-11-12 19:07:29 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -57,6 +57,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
