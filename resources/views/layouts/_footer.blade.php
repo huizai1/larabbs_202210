5b84f736448c903cd7e25e240ffe3b09bc38e01b
huizai1 <237002893@qq.com>
2022-11-13 16:22:36 +0000

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       由 <a href="{{ url('/') }}" target="_blank">辉👓</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-end"><a href="mailto:237002893@qq.com">联系我们</a></p>
+    <p class="float-end"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
