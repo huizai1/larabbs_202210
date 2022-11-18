e06cf9f63d80e84d84565eb5a3ddb006998811af
huizai1 <237002893@qq.com>
2022-11-18 05:25:31 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
