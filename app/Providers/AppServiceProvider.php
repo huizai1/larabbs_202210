af79eb9c8dc0489c3981fb9e236c22cd972c96d4
huizai1 <237002893@qq.com>
2022-11-19 12:44:17 +0000

    用户注册

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,5 +33,6 @@ class AppServiceProvider extends ServiceProvider
 
         \Illuminate\Pagination\Paginator::useBootstrap();
         //
+        JsonResource::withoutWrapping();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\VerificationCodesController;
+use App\Http\Controllers\Api\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,4 +37,8 @@ Route::prefix('v1')->name('api.v1.')->group(function () {
     // 短信验证码
     Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
         ->name('verificationCodes.store');
+
+    // 用户注册
+    Route::post('users', [UsersController::class, 'store'])
+        ->name('users.store');
 });
