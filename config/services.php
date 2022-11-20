8e1bc29f9e4344e00c38d2b3b6a8ca4ec45e716d
huizai1 <237002893@qq.com>
2022-11-20 13:03:59 +0000

    微信登录

--- config/services.php
+++ config/services.php
@@ -36,4 +36,11 @@ return [
         'key'   => env('BAIDU_TRANSLATE_KEY'),
     ],
 
+    'socialite' => [
+        'wechat' => [
+            'client_id' => env('WEIXIN_KEY'),
+            'client_secret' => env('WEIXIN_SECRET'),
+            'redirect' => env('WEIXIN_REDIRECT_URI'),
+        ],
+    ]
 ];
