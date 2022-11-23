693d8c169e1b3e9f7597d29942f82d61a08aac17
huizai1 <237002893@qq.com>
2022-11-23 12:19:41 +0000

    编辑用户信息

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\VerificationCodesController;
 use App\Http\Controllers\Api\UsersController;
 use App\Http\Controllers\Api\CaptchasController;
 use App\Http\Controllers\Api\AuthorizationsController;
+use App\Http\Controllers\Api\ImagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -80,6 +81,13 @@ Route::prefix('v1')
                     // 当前登录用户信息
                     Route::get('user', [UsersController::class, 'me'])
                         ->name('user.show');
+                     // 编辑登录用户信息
+                    Route::patch('user', [UsersController::class, 'update'])
+                        ->name('user.update');
+
+                    // 上传图片
+                    Route::post('images', [ImagesController::class, 'store'])
+                        ->name('images.store');
                 });
             });
     });
