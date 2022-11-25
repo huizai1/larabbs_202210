edb16a47136f0632baa17697d8045b6f53c2da04
huizai1 <237002893@qq.com>
2022-11-25 19:17:37 +0000

    回复列表

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,15 @@ Route::prefix('v1')
                     'index', 'show'
                 ]);
 
+                // 话题回复列表
+                Route::apiResource('topics.replies', RepliesController::class)->only([
+                    'index',
+                ]);
+
+                // 某个用户的回复列表
+                Route::get('users/{user}/replies', [RepliesController::class, 'userIndex'])
+                    ->name('users.replies.index');
+
                 // 分类列表
                 Route::apiResource('categories', CategoriesController::class)
                     ->only('index');
