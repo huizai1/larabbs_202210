7b064a5075f6a93ae9a71ee694b01255eaffa180
huizai1 <237002893@qq.com>
2022-11-25 20:00:21 +0000

    消息通知列表

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Api\ImagesController;
 use App\Http\Controllers\Api\CategoriesController;
 use App\Http\Controllers\Api\TopicsController;
 use App\Http\Controllers\Api\RepliesController;
+use App\Http\Controllers\Api\NotificationsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -121,6 +122,11 @@ Route::prefix('v1')
                     // 发布, 删除回复
                     Route::apiResource('topics.replies', RepliesController::class)->only([
                         'store', 'destroy'
+                    ]);
+
+                    // 通知列表
+                    Route::apiResource('notifications', NotificationsController::class)->only([
+                        'index'
                     ]);
                 });
             });
